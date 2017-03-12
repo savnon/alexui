935e9aa2752e6d610abce4b057c658331615b270
alex <wangpeng@live.com>
2017-03-12 19:31:17 +0800

    init project structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ require('styles/App.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+
 
 var imageDatas = require('../data/imageDatas.json');
 
@@ -21,16 +21,62 @@ imageDatas = (function genImageURL(imageDataArr) {
 })(imageDatas);
 
 
+var ImgFigure  = React.createClass({
 
+    getInitialState: function() {
+        return {liked: false};
+    },
+
+    handleClick: function(event) {
+        this.setState({liked: !this.state.liked});
+    },
+
+    render: function () {
+        var text = this.state.liked ? 'like' :'do not like';
+        return(
+
+            <div class="follow-detail">
+                <button onClick={this.handleClick}>
+                    Click this {text}
+                </button>
+            </div>
+
+
+            // <div>
+            //     <figure>
+            //         {/*<img src={this.props.data.imageURL}/>*/}
+            //         <figcaption>
+            //             <h2>{this.props.data.title}</h2>
+            //         </figcaption>
+            //     </figure>
+            //     <button>Add</button>
+            // </div>
+        )
+    }
+
+});
 
 class AppComponent extends React.Component {
   render() {
+
+      var controllerUnits = [],
+          imgFigures = [];
+
+      imageDatas.forEach(function(value){
+          imgFigures.push(<ImgFigure data={value}/>)
+      });
+
+
+
+
     return (
         <section className="stage">
             <section className="img-sec">
+                {imgFigures}
+
             </section>
             <nav className="controller-nav">
-
+                {controllerUnits}
             </nav>
         </section>
     );
